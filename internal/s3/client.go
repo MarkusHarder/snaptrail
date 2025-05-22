c0eec9e02824ce722c6c111c82b4cddd703b4199
Markus Harder <markus.harder@protonmail.com>
2025-05-22 21:36:59 +0200

    added basic s3 functionality according to amazon documentation #9
    - provides basic s3 interactions (create bucket, put/download files)
    - need to wire this into services to store/fetch images

--- internal/s3/client.go
+++ internal/s3/client.go
@@ -2,11 +2,7 @@ package s3
 
 import (
 	"context"
-	"crypto/rand"
-	"encoding/hex"
-	"fmt"
 	"snaptrail/internal/config"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
@@ -24,6 +20,7 @@ func NewS3ClientFromEnv() {
 	region := conf.S3Region
 	accessKey := conf.S3AccessKey
 	secretKey := conf.S3SecretKey
+	bucketName := conf.S3Bucket
 
 	client = s3.New(s3.Options{
 		AppID: "my-application/0.0.1",
@@ -38,52 +35,19 @@ func NewS3ClientFromEnv() {
 		}},
 	})
 
-	testBucket()
-}
-
-func testBucket() {
 	ctx := context.Background()
-	VAR_BUCKET_NAME := fmt.Sprintf("my-bucket-%s", generateRandomHash(8))
-	// Create a new Bucket
-	_, err := client.CreateBucket(ctx, &s3.CreateBucketInput{
-		Bucket: aws.String(VAR_BUCKET_NAME),
-	})
+
+	bb := BucketBasics{S3Client: client}
+
+	exists, err := bb.BucketExists(ctx, bucketName)
 	if err != nil {
-		log.Err(err).Msg("failed creating bucket")
+		log.Error().Err(err).Msg("unable to check if bucket exists")
+		return
 	}
-
-	log.Printf("Created Bucket: %s", VAR_BUCKET_NAME)
-
-	// Upload a small object
-	body := strings.NewReader(strings.TrimSpace(`
-# My object
-
-This markdown document is the content of my object.
-`))
-
-	_, err = client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket:      aws.String(VAR_BUCKET_NAME),
-		Key:         aws.String("my-object-e9be8f5c"),
-		ContentType: aws.String("text/markdown"),
-
-		Body: body,
-	})
-	if err != nil {
-		log.Err(err).Msg("error putting object")
+	if !exists {
+		err := bb.CreateBucket(ctx, bucketName, region)
+		if err != nil {
+			log.Fatal().Err(err).Msg("could not create bucket for images")
+		}
 	}
-
-	log.Info().Msg("Created object")
-}
-
-func GetS3() *s3.Client {
-	return client
-}
-
-func generateRandomHash(length int) string {
-	bytes := make([]byte, length)
-	_, err := rand.Read(bytes)
-	if err != nil {
-		log.Err(err).Msg("failed generating 8 digit hash")
-	}
-	return hex.EncodeToString(bytes)[:length]
 }
